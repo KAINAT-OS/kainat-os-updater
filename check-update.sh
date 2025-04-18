211de596ffbd2cbbc921d9392ac600d6180455db
kainatquaderee <68397736+kainatquaderee@users.noreply.github.com>
2025-04-18 23:34:45 +0600

    Update check-update.sh

--- check-update.sh
+++ check-update.sh
@@ -1,4 +1,38 @@
 #!/bin/bash
+
+# Check if the file exists and is accessible
+if curl -s -o /dev/null -w "%{http_code}\n" https://raw.githubusercontent.com/KAINAT-OS/kainat-os-updater/refs/heads/main/Packages.sh | grep -q '200'; then
+    # Fetch the content of the file and store it in a variable
+    Packages=$(curl -s https://raw.githubusercontent.com/KAINAT-OS/kainat-os-updater/refs/heads/main/Packages.sh)
+
+    # Check if the Dependencies variable exists in the file
+    if echo "$Packages" | grep -q 'Dependencies='; then
+        # Source the content of the Packages variable
+        echo "$Packages" | bash
+
+        # Split the Dependencies variable into an array using comma as the delimiter
+        IFS=',' read -ra Dependencies_array <<< "$Dependencies"
+
+        # Loop through the array and print each package name
+        for package in "${Dependencies_array[@]}"; do
+            echo "$package"
+        done
+    else
+        echo "Error: Dependencies variable not found in Packages.sh"
+    fi
+else
+    echo "Error: Packages.sh not found or inaccessible."
+fi
+
+
+
+
+
+
+
+
+
+
 #K-SETTINGS
 # source the online version
 source <(curl -s https://raw.githubusercontent.com/KAINAT-OS/K-SETTINGS/refs/heads/main/version.sh)
